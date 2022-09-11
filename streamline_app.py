6a53ba39e4d94cd93a595fb19b2d9ab43e1961b3
AKbunker <111612309+AKbunker@users.noreply.github.com>
2022-09-11 11:27:42 -0800

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -62,7 +62,7 @@ if streamlit. button('Get Fruit List'):
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into fruit load list values ('"+ my_fruit_list +"')")
+         my_cur.execute("insert into fruit load list values ('"+ (**streamlit.secrets["snowflake"]) +"')")
          return "Thanks for adding " + new_fruit
         
 #allow end user to add a fruit
