597215cc7976191b3c9f8a5fc4f3a3653d7f8b7d
AKbunker <111612309+AKbunker@users.noreply.github.com>
2022-09-11 00:50:44 -0800

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -56,7 +56,7 @@ if streamlit.button('Get Fruit Load List'):
         
 # Add a button to load the fruit
 if streamlit. button('Get Fruit List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"1)
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data rows = get_fruit_load list()
     my_cnx.close()
     streamlit.dataframe (my_data_rows)        
