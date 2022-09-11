65fb3e91e32031e2432087cbc3f724b0de22e0b5
AKbunker <111612309+AKbunker@users.noreply.github.com>
2022-09-10 20:38:04 -0800

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -14,7 +14,6 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-
 # Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
@@ -35,3 +34,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+import snowflake.connector
